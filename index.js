d64a46333c0f6841f9672dd6ac7fb85cf4814f8b
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2023-04-05 22:16:32 +0300

    Adds objects

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>");
+// console.log(template({ doesWhat: "rocks!" }));
+
 const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
 
 const findPokemonButtonEl = document.querySelector('.find_pokemon_btn');
@@ -7,11 +10,6 @@ const winnerNameEl = document.querySelector('.winner');
 const fightEl = document.querySelector('.fight_list');
 const vsLogoEl = document.querySelector('.vs_logo');
 const logoPokemonEl = document.querySelector('.logo_pokemon');
-
-winnerNameEl.style.display = "none";
-fightEl.style = "display: none;"
-startFightButtonEl.style.display = "none";
-vsLogoEl.style.display = "none";
 
 let round = 1;
 
@@ -29,6 +27,15 @@ let maxSpecialDefense2 = null;
 let useSpecialPower1 = false;
 let useSpecialPower2 = false;
 
+let pokemon1Stats = null;
+let pokemon2Stats = null;
+
+let pokemon1RoundStats = null;
+let pokemon2RoundStats = null;
+
+winnerNameEl.style.display = "none";
+fightEl.style = "display: none;"
+
 findPokemonButtonEl.addEventListener('click', onFindPokemonButtonClick);
 startFightButtonEl.addEventListener('click', onStartFightButtonClick);
 
@@ -39,11 +46,11 @@ function onFindPokemonButtonClick() {
     round = 1;
     startFightButtonEl.innerHTML = "Start a fight";
     logoPokemonEl.style.display = "none";
-    startFightButtonEl.removeAttribute('disabled');
-
+    
+    
     for (let index = 0; index < 2; index++) {
         createPokemon(index);        
-    }
+    }  
 }
 
 function onStartFightButtonClick() {
@@ -51,49 +58,28 @@ function onStartFightButtonClick() {
 
     startFightButtonEl.textContent = ` ${"Round " + round}`;
     round = round + 1;
-    
-    // const pokemonStats = [".health", ".attack", ".special_attack", ".defense", ".special_defense"]
 
-    // const pokemon1values = {
-    //     maxHealth1,
-    //     maxAttack1,
-    //     maxSpecialAttack1,
-    //     maxDefense1,
-    //     maxSpecialDefense1,
-    // }
+    maxHealth1 = pokemon1Stats.health;
+    maxAttack1 = pokemon1Stats.attack;
+    maxSpecialAttack1 = pokemon1Stats.special_attack;
+    maxDefense1 = pokemon1Stats.defense;
+    maxSpecialDefense1 = pokemon1Stats.special_defense;
 
-    // const pokemon2values = {
-    //     maxHealth2,
-    //     maxAttack2,
-    //     maxSpecialAttack2,
-    //     maxDefense2,
-    //     maxSpecialDefense2
-    // }
-    
-    // getInnerText(".pokemon1", pokemon1values, pokemonStats);
-    // getInnerText(".pokemon2", pokemon2values, pokemonStats);
+    maxHealth2 = pokemon2Stats.health;
+    maxAttack2 = pokemon2Stats.attack;
+    maxSpecialAttack2 = pokemon2Stats.special_attack;
+    maxDefense2 = pokemon2Stats.defense;
+    maxSpecialDefense2 = pokemon2Stats.special_defense;
 
-    maxHealth1 = Number(document.querySelector('.pokemon1 .health').innerText);
-    maxAttack1 = Number(document.querySelector('.pokemon1 .attack').innerText);
-    maxSpecialAttack1 = Number(document.querySelector('.pokemon1 .special_attack').innerText);
-    maxDefense1 = Number(document.querySelector('.pokemon1 .defense').innerText);
-    maxSpecialDefense1 = Number(document.querySelector('.pokemon1 .special_defense').innerText);
-
-    maxHealth2 = Number(document.querySelector('.pokemon2 .health').innerText);
-    maxAttack2 = Number(document.querySelector('.pokemon2 .attack').innerText);
-    maxSpecialAttack2 = Number(document.querySelector('.pokemon2 .special_attack').innerText);
-    maxDefense2 = Number(document.querySelector('.pokemon2 .defense').innerText);
-    maxSpecialDefense2 = Number(document.querySelector('.pokemon2 .special_defense').innerText);
-    
-    setFightStats();
-    
     let health1 = document.querySelector('.pokemon_list .health1').clientWidth;
     let health2 = document.querySelector('.pokemon_list .health2').clientWidth;
 
-    const attack1 = Number(document.querySelector('.pokemon_fight1 .attack1').innerText);
-    const defense1 = Number(document.querySelector('.pokemon_fight1 .defense1').innerText);
-    const attack2 = Number(document.querySelector('.pokemon_fight2 .attack2').innerText);
-    const defense2 = Number(document.querySelector('.pokemon_fight2 .defense2').innerText);
+    setFightStats();
+    
+    const attack1 = pokemon1RoundStats.attack;
+    const defense1 = pokemon1RoundStats.defense;
+    const attack2 = pokemon2RoundStats.attack;
+    const defense2 = pokemon1RoundStats.defense;
 
     const demage1 = attack2 - defense1;
     const demage2 = attack1 - defense2;
@@ -137,10 +123,12 @@ function onStartFightButtonClick() {
         if (health1 > health2) {
             setTimeout(() => {
                 winnerCongratulate(1);
+                startFightButtonEl.style.display = "none";
             }, 1000);
         } else {
             setTimeout(() => {
                 winnerCongratulate(2);
+                startFightButtonEl.style.display = "none";
             }, 1000);
         }
     }
@@ -154,6 +142,7 @@ function winnerCongratulate(winner) {
     
     document.querySelector('.stats2').style.display = "none";
     document.querySelector('.stats1').style.display = "none";
+
     document.querySelector('.name1').style.display = "none";
     document.querySelector('.name2').style.display = "none";
 
@@ -173,9 +162,7 @@ function winnerCongratulate(winner) {
     }
 
     looserPokemon.style.display = "none";
-
     vsLogoEl.style.display = "none";
-    
     winnerNameEl.textContent = (`${(winnerName.textContent)} wins!`).toUpperCase();
     winnerNameEl.style.display = "block";
     findPokemonButtonEl.style.display = "block";    
@@ -185,12 +172,6 @@ function winnerCongratulate(winner) {
 function createPokemon(index) {
     const randomNumber = getRandomNumber(1, 1008);
     const url = baseURL + randomNumber;
-    
-    let health = null;
-    let attack = null;
-    let defense = null;
-    let special_attack = null;
-    let special_defense = null;
 
     const xhr = new XMLHttpRequest();
 
@@ -205,28 +186,45 @@ function createPokemon(index) {
             const response = JSON.parse(xhr.response);
             const image_dream_world = response.sprites.other.dream_world.front_default;
             const image_home = response.sprites.other.home.front_default;
-            const name = response.name;
             const fight_stat = response.stats;
-    
-            health = response.base_experience;
 
-            if (!health) {
-                health = getRandomNumber(50, 250);
-            }           
+            const pokemonStats = {
+                name: response.name,
+                health: null,
+                image: null,
+                attack: null,
+                special_attack: null,
+                defense: null,
+                special_defense: null
+            }
+
+            if (image_dream_world) {
+                pokemonStats.image = image_dream_world;
+            } else if (image_home) {
+                pokemonStats.image = image_home;
+            } else {
+                pokemonStats.image = 'images/image_default.png';
+            }
+    
+            if (response.base_experience) {
+                pokemonStats.health = response.base_experience;
+            } else {
+                pokemonStats.health = getRandomNumber(50, 250);
+            }
 
             fight_stat.map(item => {
                 switch (item.stat.name) {
                     case 'attack':
-                        attack = item.base_stat;
+                        pokemonStats.attack = item.base_stat;
                         break;
                     case 'defense':
-                        defense = item.base_stat
+                        pokemonStats.defense = item.base_stat
                         break;
                     case 'special-attack':
-                        special_attack = item.base_stat
+                        pokemonStats.special_attack = item.base_stat
                         break;
                     case 'special-defense':
-                        special_defense = item.base_stat
+                        pokemonStats.special_defense = item.base_stat
                             break;
                     default:
                         break;
@@ -235,22 +233,22 @@ function createPokemon(index) {
             
             listEl.insertAdjacentHTML('beforeend',
                 `<li class=${'pokemon'+(index+1)}>
-                    <p class=${"name" + (index + 1)}>${name}</p>
+                    <p class=${"name" + (index + 1)}>${pokemonStats.name}</p>
                     <div class="wrapper">
-                        <div class=${"health"+(index+1)} style="width: ${health}px; height: 22px; position: absolute; top: 0; left: 0;"></div>
-                        <div class=${"shadow"+(index+1)} style="width: ${health}px; height: 20px; position: absolute; top: 0; left: 0; border: 1px solid black;"></div>
+                        <div class=${"health"+(index+1)} style="width: ${pokemonStats.health}px; height: 22px; position: absolute; top: 0; left: 0;"></div>
+                        <div class=${"shadow"+(index+1)} style="width: ${pokemonStats.health}px; height: 20px; position: absolute; top: 0; left: 0; border: 1px solid black;"></div>
                     </div>
-                    <img class="pokemon_image" src="${image_dream_world || image_home || 'images/image_default.png'}"/>
+                    <img class="pokemon_image" src="${pokemonStats.image}" />
                     <div class=${'"demageWrapper'+(index+1)}" style="display: none">
                         <img class=${"demageImg"+(index+1)} src="images/demage.png" alt="Demage effect">
                         <span class=${'"demageValue'+(index+1)}"></span>
                     </div>
                     <div class=${"stats"+(index+1)}>
-                    <p>Health: <span class="health">${health}</span></p>
-                    <p>Max Attack: <span class="attack">${attack}</span></p>
-                    <p>Max Special attack: <span class="special_attack">${special_attack}</span></p>
-                    <p>Max Defense: <span class="defense">${defense}</span></p>
-                    <p>Max Special defense: <span class="special_defense">${special_defense}</span></p>
+                    <p>Health: <span class="health">${pokemonStats.health}</span></p>
+                    <p>Max Attack: <span class="attack">${pokemonStats.attack}</span></p>
+                    <p>Max Special attack: <span class="special_attack">${pokemonStats.special_attack}</span></p>
+                    <p>Max Defense: <span class="defense">${pokemonStats.defense}</span></p>
+                    <p>Max Special defense: <span class="special_defense">${pokemonStats.special_defense}</span></p>
                     </div>
                 </li>`
             )
@@ -263,10 +261,21 @@ function createPokemon(index) {
             )
 
             const listChildren = listEl.childNodes.length;
+
             if (listChildren === 2) {
                 findPokemonButtonEl.style.display = "none";
                 startFightButtonEl.style.display = "block";
+                startFightButtonEl.removeAttribute('disabled');
                 vsLogoEl.style.display = "block";
+            }
+
+            switch (index) {
+                case 0:
+                    pokemon1Stats = pokemonStats;
+                    break;
+                case 1:
+                    pokemon2Stats = pokemonStats;
+                    break;
             }
         }
     }    
@@ -294,17 +303,28 @@ function setFightStats() {
         useSpecialPower2 = false;
     }
 
+
     const attack1 = useSpecialPower1 ? Math.floor((maxAttack1 + maxSpecialAttack1) * Math.random()) : Math.floor(maxAttack1 * Math.random());
     const defense1 = useSpecialPower1 ? Math.floor((maxDefense1 + maxSpecialDefense1) * Math.random()) : Math.floor(maxDefense1 * Math.random());
 
     const attack2 = useSpecialPower2 ? Math.floor((maxAttack2 + maxSpecialAttack2) * Math.random()) : Math.floor(maxAttack2 * Math.random());
     const defense2 = useSpecialPower2 ? Math.floor((maxDefense2 + maxSpecialDefense2) * Math.random()) : Math.floor(maxDefense2 * Math.random());
-    
+
     document.querySelector('.pokemon_fight1 .attack1').textContent = attack1;
     document.querySelector('.pokemon_fight1 .defense1').textContent = defense1;
 
     document.querySelector('.pokemon_fight2 .attack2').textContent = attack2;
     document.querySelector('.pokemon_fight2 .defense2').textContent = defense2;
+
+    pokemon1RoundStats = {
+        attack: attack1,
+        defense: defense1
+    };
+
+    pokemon2RoundStats = {
+        attack: attack2,
+        defense: defense2
+    };
 
     useSpecialPower1 = false;
     useSpecialPower2 = false;
